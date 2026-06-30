4c1c8bd483db934d225d377a889c1cac98af4d99
Cj_2444 <cj.c.marambio@gmail.com>
2026-06-30 11:12:34 -0400

    Añadir a App la referencia a game.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
+import { ref } from 'vue'
+
 import MainMenu from './components/mainMenu.vue'
+import Game from './components/game.vue'
+
+const gameStarted = ref(false)
+
+function startGame() {
+  gameStarted.value = true
+}
 </script>
 
 <template>
   <MainMenu v-if="!gameStarted" @start="startGame" />
+
+  <Game v-else />
 </template>
 
 <style scoped></style>
