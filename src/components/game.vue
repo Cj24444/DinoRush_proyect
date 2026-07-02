2cff3e094e43098a4619564c4ebf8e7a55686df8
Cj_2444 <cj.c.marambio@gmail.com>
2026-07-01 21:01:36 -0400

    Actualizar game para mostrar al jugador, con estilo básico

--- src/components/game.vue
+++ src/components/game.vue
@@ -1,5 +1,67 @@
+<script setup>
+
+import { onMounted, onUnmounted } from "vue"
+import { useGame } from "./usingGame"
+
+const {
+    playerY,
+    jump
+} = useGame()
+
+
+function keyDown(e){
+    if(e.code === "Space" || e.code === "ArrowUp"){
+        jump()
+    }
+}
+
+onMounted(()=>{
+    window.addEventListener("keydown",keyDown)
+})
+
+onUnmounted(()=>{
+    window.removeEventListener("keydown",keyDown)
+})
+</script>
+
 <template>
   <div class="game">
-    <h2>Juego en desarrollo, vuelva más tarde</h2>
+      <div class="ground"></div>
+      <div
+          class="player"
+          :style="{ bottom: `${playerY}px` }"
+      ></div>
   </div>
 </template>
+
+
+
+<style scoped>
+
+.game{
+    position:relative;
+    width:900px;
+    height:400px;
+    margin:auto;
+    overflow:hidden;
+    background:#ccefff;
+
+}
+
+.ground{
+    position:absolute;
+    width:100%;
+    height:40px;
+    background:#6f4e37;
+    bottom:0px;
+}
+
+.player{
+    position:absolute;
+    left:100px;
+    width:40px;
+    height:40px;
+    background:#2ecc71;
+}
+
+</style>
